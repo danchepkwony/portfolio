bea206a4a43942b0d2d6d42ff9227f0715f1d700
Dan Chepkwony <dan.k.chepkwony@gmail.com>
2020-07-11 15:58:52 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Main">
+      <header className="Main-header">
+        This is my portfolio. :)
+        I haven't actually added anything here yet.
+        I will soon.
+      </header>
+    </div>
+  );
+}
+
+export default App;
