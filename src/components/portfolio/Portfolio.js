4835c0f297124aeeb696a4704be086b5eb4b7f53
Dan Chepkwony <dan.k.chepkwony@gmail.com>
2021-05-01 22:10:08 -0400

    Fix mobile

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -11,55 +11,35 @@ import projects from '../../projects.json';
 
 const PortfolioPage = ( props ) => {
 
-  //TODO: This is so gross
   var Projects = {communicaid: <Project title = {projects.Communicaid.title} description = {projects.Communicaid.description} skills = {projects.Communicaid.skills} img = {communicaid} hasLink = {true} link = {projects.Communicaid.link}/>,
                 healthradar: <Project title = {projects.HealthRadar.title} description = {projects.HealthRadar.description} skills = {projects.HealthRadar.skills} img = {healthradar} hasLink = {true} link = {projects.HealthRadar.link} />,
                 dashboard: <Project title = {projects.Basecamp.title} description = {projects.Basecamp.description} skills = {projects.Basecamp.skills} img = {dashboard}/>,
                 statusboard: <Project title = {projects.StatusBoard.title} description = {projects.StatusBoard.description} skills = {projects.StatusBoard.skills} img = {status}/>
   }
 
-//TODO: Clean this up
+  //TODO: Make
   return (
     <div className = "Portfolio">
-      { props.project == null && props.wideEnough ?
+      { props.project == null ?
       <div className = "Links">
-        <div className = "project">
-          <div className = "hover-container">
-            <div className = "container" id="communicaid">
-              <img className = "arrow" src={arrow} alt=""/>
-            </div>
-            <NavLink to="/portfolio/communicaid" activeClassName="link-active" className = "link">COMMUNICAID</NavLink>
-          </div>
-        </div>
-        <div className = "project">
-          <div className = "hover-container">
-            <div className = "container" id="health">
-              <img className = "arrow"src={arrow} alt=""/>
-            </div>
-            <NavLink to="/portfolio/health-radar" activeClassName="link-active" className = "link">MY HEALTH RADAR</NavLink>
-          </div>
-        </div>
-        <div className = "project">
-          <div className = "hover-container">
-            <div className = "container" id="basecamp">
-              <img className = "arrow"src={arrow} alt=""/>
-            </div>
-            <NavLink to="/portfolio/dashboard" activeClassName="link-active" className = "link">BASECAMP DASHBOARD</NavLink>
-          </div>
-        </div>
-        <div className = "project">
-          <div className = "hover-container">
-            <div className = "container" id="status">
-              <img className = "arrow"src={arrow} alt=""/>
-            </div>
-            <NavLink to="/portfolio/status-board" activeClassName="link-active" className = "link">STATUS BOARD</NavLink>
-          </div>
-        </div>
+        <ProjectLink id = "communicaid" wideEnough = {props.wideEnough} lineColor="orange" />
+        <ProjectLink id = "health-radar" wideEnough = {props.wideEnough} lineColor="yellow"/>
+        <ProjectLink id = "dashboard" wideEnough = {props.wideEnough} lineColor="orange"/>
+        <ProjectLink id = "status-board" wideEnough = {props.wideEnough} lineColor="yellow"/>
       </div>: <>{Projects[props.project]}</> }
-    { !props.wideEnough ? <div className = "Portfolio-Mobile">
-    </div> : ""}
     </div>
   )
 }
+
+const ProjectLink = (props) => (
+    <div className = {props.wideEnough ? "project" : "project-mobile"}>
+      <div className = "hover-container">
+        <div className = "container" id={props.id}>
+          <img className = "arrow" src={arrow} alt=""/>
+        </div>
+        <NavLink to={"/portfolio/" + props.id} activeClassName="link-active" className = {"link " + props.lineColor}>{props.id.toUpperCase().replace('-', ' ')}</NavLink>  
+      </div>
+    </div>
+)
 
 export default PortfolioPage;
--- src/components/portfolio/project/Project.js
+++ src/components/portfolio/project/Project.js
@@ -3,19 +3,19 @@ import './Project.css';
 
 const Project = (props) => (
     <div className = "Project">
-      <div className = "header">
-        <h1 className = "title">{props.title}</h1>
-        <ul className = "skills">
-              {props.skills.map(skill => <li key={skill} className ="skill">{skill}</li>)}
-          </ul>
-      </div>
       <div className="body">
         <div className = "container">
           <img className = "image" src = {props.img} alt={"Image of " + props.title}/>
         </div>
         <div className = "text">
+          <div className = "header">
+            <h1 className = "title">{props.title}</h1>
+            <ul className = "skills">
+                  {props.skills.map(skill => <li key={skill} className ="skill">{skill}</li>)}
+              </ul>
+          </div>
           <p className = "details">{props.description}</p>
-          { props.hasLink ? <a className = "link" href={props.link}>View Project</a> : ""}
+          { props.hasLink ? <a className = "link" href={props.link} target="_blank" rel="noopener noreferrer">View Project</a> : ""}
         </div>
       </div>
     </div>
