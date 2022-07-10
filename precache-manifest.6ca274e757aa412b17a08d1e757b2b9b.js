cea760b34c851e218fbfb6d79c16bbe41e9affd3
Dan Chepkwony <danchepkwony@Dans-Air.lan>
2022-07-09 22:37:32 -0400

    Updates

---
+++ precache-manifest.6ca274e757aa412b17a08d1e757b2b9b.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a1cd60c70de5250da5d472a3436714",
+    "url": "/danchepkwony/portfolio/index.html"
+  },
+  {
+    "revision": "a04c71f8a31b93b959f1",
+    "url": "/danchepkwony/portfolio/static/css/main.91bc462d.chunk.css"
+  },
+  {
+    "revision": "6a1fb24af0a348da507d",
+    "url": "/danchepkwony/portfolio/static/js/2.85d7d4e1.chunk.js"
+  },
+  {
+    "revision": "943882469e83e6eb295f46d8b1eb0b5b",
+    "url": "/danchepkwony/portfolio/static/js/2.85d7d4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04c71f8a31b93b959f1",
+    "url": "/danchepkwony/portfolio/static/js/main.bb787f19.chunk.js"
+  },
+  {
+    "revision": "52f728a006f303e60e3e",
+    "url": "/danchepkwony/portfolio/static/js/runtime-main.8d0fbe2d.js"
+  },
+  {
+    "revision": "04c38a99d8afcbb092591a7a88a39a88",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif-Bold.04c38a99.ttf"
+  },
+  {
+    "revision": "250e2c4256e6e1f34fa812e684d27d2c",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif-Bold.250e2c42.woff2"
+  },
+  {
+    "revision": "2a00f85c873bf26a65b53e7be1e3ef36",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif-Bold.2a00f85c.svg"
+  },
+  {
+    "revision": "39c0e013fbe0351a411bcb13a5e660e8",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif-Bold.39c0e013.woff"
+  },
+  {
+    "revision": "f877257d9f6844636b23d4d438a852b6",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif-Bold.f877257d.eot"
+  },
+  {
+    "revision": "2d5af38fb4eb9db931dee94d1ddc72e7",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif-Italic.2d5af38f.eot"
+  },
+  {
+    "revision": "6fb3daeba810b8fff996a227f8ef176a",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif-Italic.6fb3daeb.woff"
+  },
+  {
+    "revision": "a28604a01636f3a9249559626bc6e5ab",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif-Italic.a28604a0.ttf"
+  },
+  {
+    "revision": "a98256a8e312a402e4df0a707100a6db",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif-Italic.a98256a8.woff2"
+  },
+  {
+    "revision": "be7e27fb786856d96700eaabd26b9444",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif-Italic.be7e27fb.svg"
+  },
+  {
+    "revision": "0e38816d03bc599c85dd38f41fa1a049",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif.0e38816d.svg"
+  },
+  {
+    "revision": "4d49be6e42d18464b230d87d1fab175a",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif.4d49be6e.ttf"
+  },
+  {
+    "revision": "864eb0eb96ebcd835cc2e1e7a5aa0b60",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif.864eb0eb.woff2"
+  },
+  {
+    "revision": "b3ea99fd8c6bfebc520a72d37f61f042",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif.b3ea99fd.eot"
+  },
+  {
+    "revision": "e5d90a88502f7803b9b228e58e505545",
+    "url": "/danchepkwony/portfolio/static/media/Kollektif.e5d90a88.woff"
+  },
+  {
+    "revision": "e019cc53409ad50446716573a015eb20",
+    "url": "/danchepkwony/portfolio/static/media/arrow.e019cc53.svg"
+  },
+  {
+    "revision": "64acf34c0dca8467e56fb176e1164ad2",
+    "url": "/danchepkwony/portfolio/static/media/codepen.64acf34c.svg"
+  },
+  {
+    "revision": "db864700cf80e872e82147713ddeb69c",
+    "url": "/danchepkwony/portfolio/static/media/devpost.db864700.svg"
+  },
+  {
+    "revision": "5b1d13c4f20ef8bfdd2563cb84ce5836",
+    "url": "/danchepkwony/portfolio/static/media/github.5b1d13c4.svg"
+  },
+  {
+    "revision": "e02630168d7c83efc45aa082ab4ff12a",
+    "url": "/danchepkwony/portfolio/static/media/lsamp.5066e9cc.png"
+  },
+  {
+    "revision": "f135d907688065d2b5e47f305a0db444",
+    "url": "/danchepkwony/portfolio/static/media/lsamp2.c6037e73.png"
+  },
+  {
+    "revision": "126812cd472e8fdf5a52eaa9432d5c00",
+    "url": "/danchepkwony/portfolio/static/media/maarc.035a25b2.png"
+  },
+  {
+    "revision": "864e1585037d3b1717bb74f7e269f46f",
+    "url": "/danchepkwony/portfolio/static/media/maarc2.44bda214.png"
+  },
+  {
+    "revision": "d09578dbc1a27ee5dded6bf4bb80413a",
+    "url": "/danchepkwony/portfolio/static/media/mouse.d09578db.svg"
+  },
+  {
+    "revision": "b5728b870f41b805c64a016f39ca073f",
+    "url": "/danchepkwony/portfolio/static/media/puzzle.f5d6b11a.png"
+  },
+  {
+    "revision": "28daa540457b745499f8467bb4dcae03",
+    "url": "/danchepkwony/portfolio/static/media/puzzle2.7066092d.png"
+  },
+  {
+    "revision": "d030539d2d7354020f1d3d7840413615",
+    "url": "/danchepkwony/portfolio/static/media/slack.1de63a5d.png"
+  },
+  {
+    "revision": "b673cc8da3af1b5c33321d61513b975e",
+    "url": "/danchepkwony/portfolio/static/media/slack2.58b41b3d.png"
+  },
+  {
+    "revision": "1cc30dd2c05688b68c693353a48594ed",
+    "url": "/danchepkwony/portfolio/static/media/sportslink.ec1228eb.png"
+  },
+  {
+    "revision": "95ea4a2983a466bc74460b98f9ecc199",
+    "url": "/danchepkwony/portfolio/static/media/sportslink2.e9db54d5.png"
+  },
+  {
+    "revision": "0af9733dd3d41cad2a0c8253d34e5bfb",
+    "url": "/danchepkwony/portfolio/static/media/techpoint.04016ae4.png"
+  },
+  {
+    "revision": "65e9ea6f1f388fe80bf3ecfa18bb93fe",
+    "url": "/danchepkwony/portfolio/static/media/techpoint2.14686edb.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.666f54aa3145cbb8d804fecbbf966d88.js"
+  "/danchepkwony/portfolio/precache-manifest.6ca274e757aa412b17a08d1e757b2b9b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/danchepkwony/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
